98d8c4d0904846e056745693e8fd77963def054e
Tường Hoàng <mr.tuongqh1@gmail.com>
2022-07-22 03:25:38 +0700

    initial commit

---
+++ database.sql
@@ -0,0 +1,10 @@
+create database test;
+
+use test;
+
+CREATE TABLE employees (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    salary INT(10) NOT NULL
+);
